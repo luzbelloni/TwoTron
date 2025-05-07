3bbc23e306ed0426db33bfc4cf8afdb21c95104d
Luz Belloni <luz.belloni@twofold.swiss>
2025-05-07 14:20:19 +0200

    created and configured main game class

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
-console.log(document.getElementById('canvas'));
+var canvas = document.getElementById("canvas");
+
+canvas.width = 800;
+canvas.height = 600;
+
+class BeamWars {
+  constructor(canvas) {
+    this.canvas = canvas;
+    this.width = this.canvas.width;
+    this.height = this.canvas.height;
+
+    this.ctx = this.canvas.getContext("2d");
+
+    this.ctx.fillRect(0, 0, this.width, this.height);
+  }
+}
+
+
+const Game = new BeamWars(canvas);
